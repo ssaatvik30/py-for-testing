7836ad513a9b0975c7190a267556119ad4a3cc16
ssaatvik30 <ssaatvik30@gmail.com>
2026-02-28 01:19:25 +0530

    testing proj

---
+++ test_math_ops.py
@@ -0,0 +1,14 @@
+import unittest
+from math_ops import add_num, sub_num
+
+class TestMathOps(unittest.TestCase):
+    
+    def test_add(self):
+        self.assertEqual(add_num(2, 3), 5)
+        self.assertEqual(add_num(-1, 1), 0)
+
+    def test_subtract(self):
+        self.assertEqual(sub_num(10, 5), 5)
+
+if __name__ == '__main__':
+    unittest.main()
